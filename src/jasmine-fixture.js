b2ab4b50538b4e1da9d7c032491fdfb5bf6a63ae
Massimo Foti <massimo.foti@appway.com>
2017-12-19 21:18:49 +0100

    Base implementation of .readFixture()

--- src/jasmine-fixture.js
+++ src/jasmine-fixture.js
@@ -8,9 +8,59 @@ if(typeof(window.jasmineFixture) === "undefined"){
 	window.jasmineFixture = {};
 }
 
+/**
+ * @typedef {Object} jasmineFixture.options
+ *
+ * @property {String} basePath        Base path for fixtures. Default to "fixtures/"
+ */
+
 (function(){
 	"use strict";
 
 	jasmineFixture.version = "0.1";
+
+	/**
+	 * @type {jasmineFixture.options}
+	 */
+	var config = {
+		basePath: "fixtures/"
+	};
+
+	/**
+	 * @type {Object.<String, String>}
+	 */
+	jasmineFixture.cache = {};
+
+	jasmineFixture.clearCache = function(){
+		jasmineFixture.cache = {};
+	};
+
+	/**
+	 * @param {String} url
+	 * @returns {String}
+	 */
+	jasmineFixture.readFixture = function(url){
+		var fullUrl = assembleUrl(url);
+		if(jQuery.type(jasmineFixture.cache[fullUrl]) === "undefined"){
+			readIntoCache(fullUrl);
+		}
+		return jasmineFixture.cache[fullUrl];
+	};
+
+	var assembleUrl = function(url){
+		return config.basePath + url;
+	};
+
+	var readIntoCache = function(url){
+		jQuery.ajax({
+			url: url,
+			async: false, // Must be synchronous to ensure fixtures are loaded before test run
+			cache: false
+		}).done(function(data, textStatus, jqXHR){
+			jasmineFixture.cache[url] = data;
+		}).fail(function(jqXHR, textStatus, errorThrown){
+			throw ("Failed to retrieve fixture at: " + url + " (status: " + jqXHR.status + ")");
+		});
+	};
 
 }());
--- test/spec/jasmineFixture.Spec.js
+++ test/spec/jasmineFixture.Spec.js
@@ -2,9 +2,14 @@ describe("jasmineFixture", function(){
 
 	"use strict";
 
+	var firstText, secondText;
 	beforeEach(function(){
+		firstText = "<div>Test1</div>";
+		secondText = "<div>Test2</div>";
+	});
 
-
+	afterEach(function(){
+		jasmineFixture.clearCache();
 	});
 
 	it("Requires jQuery in order to work", function(){
@@ -16,9 +21,81 @@ describe("jasmineFixture", function(){
 	});
 
 	describe(".version", function(){
+
 		it("Reports the current version number", function(){
 			expect(jasmineFixture.version).toBeDefined();
 		});
+
+	});
+
+	describe(".cache", function(){
+
+		it("Is a plain object", function(){
+			expect(jQuery.isPlainObject(jasmineFixture.cache)).toEqual(true);
+		});
+
+		it("Contains no keys out of the box", function(){
+			expect(Object.keys(jasmineFixture.cache).length).toEqual(0);
+		});
+
+		it("Contains one key for each retrieved fixture", function(){
+			jasmineFixture.readFixture("first.htm");
+			expect(Object.keys(jasmineFixture.cache).length).toEqual(1);
+			jasmineFixture.readFixture("second.htm");
+			expect(Object.keys(jasmineFixture.cache).length).toEqual(2);
+		});
+
+		it("Each key map an url to its response text", function(){
+			jasmineFixture.readFixture("first.htm");
+			expect(jasmineFixture.cache["fixtures/first.htm"]).toEqual(firstText);
+			jasmineFixture.readFixture("second.htm");
+			expect(jasmineFixture.cache["fixtures/second.htm"]).toEqual(secondText);
+		});
+
+	});
+
+	describe(".clearCache()", function(){
+
+		it("Flush/clear the cache", function(){
+			jasmineFixture.readFixture("first.htm");
+			expect(Object.keys(jasmineFixture.cache).length).toEqual(1);
+			jasmineFixture.clearCache();
+			expect(Object.keys(jasmineFixture.cache).length).toEqual(0);
+		});
+
+	});
+
+	describe(".readFixture()", function(){
+
+		beforeEach(function(){
+			spyOn(jQuery, "ajax").and.callThrough();
+		});
+
+		it("Returns the given fixture as string", function(){
+			expect(jasmineFixture.readFixture("first.htm")).toEqual(firstText);
+			expect(jasmineFixture.readFixture("second.htm")).toEqual(secondText);
+			expect(jQuery.type(jasmineFixture.readFixture("first.htm"))).toEqual("string");
+		});
+
+		it("Invokes jQuery.ajax to retrieve the fixture", function(){
+			jasmineFixture.readFixture("first.htm");
+			expect(jQuery.ajax).toHaveBeenCalled();
+		});
+
+		it("Only one XHR call is executed if the same fixture is read multiple times", function(){
+			jasmineFixture.readFixture("first.htm");
+			jasmineFixture.readFixture("first.htm");
+			expect(jQuery.ajax).toHaveBeenCalledTimes(1);
+			jasmineFixture.readFixture("second.htm");
+			expect(jQuery.ajax).toHaveBeenCalledTimes(2);
+		});
+
+		it("Throws an error if the XHR call fails", function(){
+			expect(function(){
+				jasmineFixture.readFixture("missing.htm");
+			}).toThrow();
+		});
+
 	});
 
 });
