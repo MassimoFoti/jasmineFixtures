9e37886b173b98fcb09000c3ee1b70ccfcf35787
Massimo Foti <massimo.foti@appway.com>
2018-04-06 08:11:21 +0200

    Updated ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -92,11 +92,12 @@ module.exports = {
 				},
 				preferType: {
 					// Use the right string instead of the left one
-					'object': 'Object',
-					'boolean': 'Boolean',
-					'number': 'Number',
-					'string': 'String',
-					'array': 'Array'
+					"object": "Object",
+					"boolean": "Boolean",
+					"number": "Number",
+					"function": "Function",
+					"string": "String",
+					"array": "Array"
 				},
 
 				// If and only if the function or method has a return statement (this option value does apply to constructors)
@@ -108,8 +109,8 @@ module.exports = {
 		],
 
 		// IE11 specific rules
-		'ie11/no-for-in-const': ['error'], // Error in IE11
-		'ie11/no-weak-collections': ['error'], // Error in IE11
-		'ie11/no-collection-args': ['error']
+		"ie11/no-for-in-const": ["error"], // Error in IE11
+		"ie11/no-weak-collections": ["error"], // Error in IE11
+		"ie11/no-collection-args": ["error"]
 	}
 };
